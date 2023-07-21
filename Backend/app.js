4fcacc9fb3a659df1fdd8da4350cb5194f2a9b37
Boldizsar <boldizsar.kormendy@gmail.com>
2023-07-21 21:58:08 +0200

    Added tests and CRUD functionality

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,40 @@
 //app.js
+function newId(array){
+    if (array.length > 0) {
+      return array[array.length-1].id + 1;
+    } else {
+      return 1
+    }
+  }
+
 class MessageApp {
     constructor() {
       this.messages = []
     }
-  }
+    // C
+    post(content){
+        let item = {
+            id: newId(this.messages),
+            content: content,
+            date: new Date()
+        }
+        this.messages.push(item)
+        return this.messages
+    }
+    // R
+    get(id){
+    return this.messages.filter(message => message.id === id)[0]
+    }
+    // U
+    update(id, update){
+    let index = this.messages.findIndex(message => message.id === id )
+    this.messages[index].content = update
+    }
+    // D
+    delete(id){
+    this.messages = this.messages.filter(message => message.id != id)
+    return this.messages
+    }
+   }
+
   export default MessageApp
