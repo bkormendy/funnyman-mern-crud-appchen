a677d4a6e4b5a897268990d6c2d15887c38fc87c
Boldizsar <boldizsar.kormendy@gmail.com>
2023-07-24 17:49:43 +0200

    Set up Express. I gotta start writing better commit messages.

--- Backend/app.js
+++ Backend/app.js
@@ -1,92 +1,12 @@
-import fs from 'fs'
-import path from 'path'
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 //app.js
-function newId(array){
-    if (array.length > 0) {
-      return array[array.length-1].id + 1;
-    } else {
-      return 1
-    }
-  }
+import express from 'express'
 
-class MessageApp {
-    constructor(filepath) {
-        this.filepath = filepath
-        this.messages = filepath ? this.readFromJson() : []
-    }
+const app = express()
 
-    // C
-    post(content) {
-        if (content) {
-            this.messages.push({
-                content: content,
-                date: new Date(),
-                id: newId(this.messages)
-            })
-        this.writeToJson()
-        return this.messages
-        }
-        else if (!content){
-            return []
-        }
-    }
+app.get('/', function (req, res) {
+    res.send({val:'Hello World'})
+})
 
-    // R
-    get(id){
-    return this.messages.filter(message => message.id === id)[0]
-    }
+app.listen(3001)
 
-    getAll(){
-        return this.messages
-    }
-
-    // U
-    update(id, update){
-        let index = this.messages.findIndex(message => message.id == id )
-        if (index >= 0) {
-            this.messages[index].content = update
-            this.writeToJson()
-            return this.messages
-        }
-        else {
-          return []
-        }
-    }
-
-    // D
-    delete(id) {
-        let index = this.messages.findIndex(message => message.id == id )
-        if (index >= 0) {
-            this.messages.splice(index, 1);
-            this.writeToJson()
-            return this.messages
-        }
-        else {
-            return "Message not found in database"
-        }
-    }
-
-   readFromJson(){
-    return JSON.parse(fs.readFileSync(
-      __dirname+path.normalize(this.filepath),"utf8",(err,data)=>{
-      if (err) throw err
-      })
-    )
-    }
-
-    writeToJson(){
-        if (this.filepath) {
-        const jsonItem = JSON.stringify(this.messages)
-        fs.writeFileSync(__dirname+path.normalize(this.filepath), jsonItem, (err) => {
-            if (err) throw err;
-        });
-        }
-    }
-}
-
-export default MessageApp
+export default app
